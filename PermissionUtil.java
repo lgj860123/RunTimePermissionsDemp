74cb97c9e3ab3bb05a904f8413edd079eaec8490
lgj860123 <304035749@qq.com>
2018-03-26 00:09:05 +0800

    实现功能

---
+++ PermissionUtil.java
@@ -0,0 +1,24 @@
+package com.example.luogj.runtimepermissionsdemp;
+
+import android.content.pm.PackageManager;
+
+/**
+ * Created by Administrator on 2018/3/25.
+ */
+
+public class PermissionUtil {
+    public static boolean verifyPermissions(int[] grantResults) {
+        // At least one result must be checked.
+        if (grantResults.length < 1) {
+            return false;
+        }
+
+        // Verify that each required permission has been granted, otherwise return false.
+        for (int result : grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
